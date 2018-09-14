e7d73b65e9b87846c743b653c6b2a0c21a4a737e
Devan Grose <devangrose@gmail.com>
2018-09-14 14:44:40 -0700

    finished boiler

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,36 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { toggleTodo } from '../actions/todoActions';
 
 class Todos extends Component {
-  state = {
-    todos: [
-      {
-        id: 0,
-        task: 'Test this Todo Page',
-        completed: false
-      },
-      {
-        id: 1,
-        task: 'Learn Redux',
-        completed: false
-      },
-      {
-        id: 2,
-        task: 'Learn React',
-        completed: true
-      }
-    ]
-  }
   render () {
     return (
       <div>
         <ul>
-          {this.state.todos.map(todo => (
-            <li key={todo.id}>{todo.task}: {todo.completed.toString()}</li>
+          {this.props.todos.map(todo => (
+            <li key={todo.id} onClick={() => this.props.kitten(todo.id)}>{todo.task}: {todo.completed.toString()}</li>
           ))}
         </ul>
       </div>
-    )
+    );
   }
 }
 
-export default Todos
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  };
+};
+
+export default connect(mapStateToProps, {kitten:toggleTodo} )(Todos);
